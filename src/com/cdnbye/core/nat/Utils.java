2a2bdd0b47f979aa59a3dfa552b85762af4b7a55
snowinszu <86755838@qq.com>
2023-03-01 14:01:13 +0800

    remove ParseIpAddress

--- src/com/cdnbye/core/nat/Utils.java
+++ src/com/cdnbye/core/nat/Utils.java
@@ -7,20 +7,6 @@ public class Utils {
 
     private static final String DEFAULT_LOCAL_END = "0.0.0.0";
 
-    public static InetSocketAddress ParseIpAddress(String str)
-    {
-        String[] ipPort = str.trim().split(":");
-
-        if (ipPort.length == 2)
-        {
-            return new InetSocketAddress(ipPort[0], Integer.parseInt(ipPort[1]));
-        } else if (ipPort.length == 1) {
-            return new InetSocketAddress(DEFAULT_LOCAL_END, Integer.parseInt(ipPort[0]));
-        }
-
-        return null;
-    }
-
     public static DatagramSocket createSocket(InetSocketAddress addr) throws SocketException {
 
         return new DatagramSocket(addr);
