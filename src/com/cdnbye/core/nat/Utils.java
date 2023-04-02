70f34d8ef001f6e213c93a56beb8a09914b780a5
snowinszu <86755838@qq.com>
2023-04-02 23:25:16 +0800

    rm DEFAULT_LOCAL_END

--- src/com/cdnbye/core/nat/Utils.java
+++ src/com/cdnbye/core/nat/Utils.java
@@ -4,8 +4,6 @@ import java.net.*;
 
 public class Utils {
 
-    private static final String DEFAULT_LOCAL_END = "0.0.0.0";
-
     public static byte[] ipToBytes(String ip) throws UnknownHostException {
         return InetAddress.getByName(ip).getAddress();
     }
